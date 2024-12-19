d7b474a9c05c67a834a89fcf61db442c71cf069e
이준희 <0210leejun@gmail.com>
2024-12-19 14:06:56 +0900

    commit A

--- software_engineering/src/java/A.java
+++ software_engineering/src/java/A.java
@@ -4,10 +4,10 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("MyClass");
-<<<<<<< HEAD
+
 		System.out.println("modify A in master");
-=======
+
 		System.out.println("modify A in devB");
->>>>>>> refs/heads/devB
+
 	}
 }
